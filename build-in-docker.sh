6e72f74f6d7cc2ce94417cbc08d078530b6ae16b
Yusuke Ishiguro <ishiguro_yusuke@cyberagent.co.jp>
2023-02-14 16:59:29 +0900

    日本語用ディレクトリに分離

--- build-in-docker.sh
+++ build-in-docker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-[ ! -z $REVIEW_CONFIG_FILE ] || REVIEW_CONFIG_FILE=config.yml
+[ ! -z $REVIEW_LANG ] || REVIEW_LANG=jp
+[ ! -z $REVIEW_CONFIG_FILE ] || REVIEW_CONFIG_FILE="config-$REVIEW_LANG.yml"
 
 # コマンド手打ちで作業したい時は以下の通り /book に pwd がマウントされます
 # docker run -i -t -v $(pwd):/book vvakame/review:5.1 /bin/bash
